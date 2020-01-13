557b4c41169dec4185c2acc075db7112e0cbb9ec
unknown <sun4131@gmail.com>
2020-01-13 10:12:17 +0900

    Web_Crawling_2020.01.13(Create history : -)

--- Crawling/json01.py
+++ Crawling/json01.py
@@ -2,10 +2,26 @@
 # 폴더 생성 : resources
 
 import json
+import cx_Oracle as oci
 
-file1 = open('./resources/exam01.json')
+# 접속하기 (계정/비밀번호@서버주소:포트번호/SID,encoding="utf-8"(인코딩) )
+conn = oci.connect('admin/1234@192.168.99.100:32764/xe', encoding="utf-8")
+# 커서 생성
+cursor = conn.cursor()
+
+# 파일 읽기
+file1 = open('./resources/exam02.json')
 # str to dict로 변경 
 data1 = json.load(file1)
+# {"ID", "aaa", "PW":"34"}
+
+sql = """
+INSERT INTO MEMBER(ID, PW, NAME, AGE, JOINDATE)
+VALUES(:ID, :PW , :NAME, :AGE, SYSDATE)
+"""
+
+cursor.execute(sql, data1)
+conn.commit()
 
 
 #{'str': 'hello', 'number': 34, 'array': [1, 2, 3, 4, 5], 'object': {'name': 'a', 'age': 23}, 'bool': True}
--- Crawling/oracle01.py
+++ Crawling/oracle01.py
@@ -2,8 +2,8 @@
 
 import cx_Oracle as oci
 
-# 접속하기 (암호)
-conn = oci.connect('admin/1234@192.168.99.100:32764/xe')
+# 접속하기 (계정/비밀번호@서버주소:포트번호/SID,encoding="utf-8"(인코딩) )
+conn = oci.connect('admin/1234@192.168.99.100:32764/xe', encoding="utf-8")
 # 커서 생성
 cursor = conn.cursor()
 
@@ -12,3 +12,13 @@ sql = "SELECT * FROM MEMBER"
 cursor.execute(sql)
 data = cursor.fetchall() #[(),(),()]
 print(data)
+
+
+sql = """
+INSERT INTO MEMBER(ID, PW, NAME, AGE, JOINDATE)
+VALUES(:1, :2 , :3, :4, SYSDATE)
+"""
+
+arr = ['a1', 'a1', '홍길동', 33] 
+cursor.execute(sql, arr)
+conn.commit()
