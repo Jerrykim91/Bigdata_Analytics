7ca0ae6ed15f02b23ed0789598ac0efda70b804b
jerrykim91 <sun4131@gmail.com>
2020-01-14 16:03:56 +0900

    Web_Crawling_2020.01.14(Create history : json trainning)

--- Crawling/json/json06.py
+++ Crawling/json/json06.py
@@ -4,9 +4,8 @@
 
 # import 
 # 뭐뭐 인포트 해야하나 
-import requests 
-import pymongo 
-import json
+import requests, pymongo, json
+
 
 # 몽고디비에 접속 
 conn = pymongo.MongoClient('192.168.99.100',32766)
@@ -24,7 +23,8 @@ dailyBoxOfficeList{
     4. salesAcc,
     5. scrnCnt,
     6. showCnt
-    } 출력 !! 
+    }
+출력 !! 
 """
 
 print('-'*40)
@@ -40,32 +40,42 @@ data = json.loads(str1)
 # 내가 원하는 데이터는 showRange 데이터 값을 출력 
 # dailyBoxOfficeList 안의 데이터를 출력 
 data1 = json.loads(str1)['boxOfficeResult']['dailyBoxOfficeList']  
-
+data2 = json.loads(str1)['boxOfficeResult']
+data3 = json.loads(str1)['boxOfficeResult']['showRange']
 
 # 타입 출력 해보기 
-# print(data)
-print(type(data)) # dict로 바뀐거 확인 
+# print(type(data2)) # dict로 바뀐거 확인 
+
+
 
 # 일단 for 문 돌려보자 
-for tmp in data1 :
+for tmp in data2['dailyBoxOfficeList'] :
     # print(tmp['showRange'])
     # print(tmp['dailyBoxOfficeList'])
     # print(tmp['boxofficeType'])
     print('-'*20,'Checkpoint3','-'*20)
-    # dict1 = dict()
-    # dict1[] = tmp[]
-    # print(tmp)
-    print(tmp['rankOldAndNew'])
-    print(tmp['movieNm'])
-    print(tmp['salesShare'])
-    print(tmp['salesAcc'])
-    print(tmp['scrnCnt'])
-    print(tmp['showCnt'])
 
+#     print(tmp)
+#     print(tmp['rankOldAndNew'])
+#     print(tmp['movieNm'])
+#     print(tmp['salesShare'])
+#     print(tmp['salesAcc'])
+#     print(tmp['scrnCnt'])
+#     print(tmp['showCnt'])
 
+# print('finish')    
+# print(data2['showRange'])
 
+    dict1 = dict()
+    dict1['showRange'] =  data2['showRange']
+    dict1['rankOldAndNew'] = tmp['rankOldAndNew']
+    dict1['movieNm'] = tmp['movieNm']
+    dict1['salesShare'] = tmp['salesShare']
+    dict1['salesAcc'] = tmp['salesAcc']
+    dict1['scrnCnt'] = tmp['scrnCnt']
+    dict1['showCnt'] = tmp['showCnt']
 
-
+    table.insert_one(dict1)
     # 1. rankOldAndNew,
     # 2. movieNm,
     # 3. salesShare,
