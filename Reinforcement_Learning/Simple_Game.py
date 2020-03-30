2c9b349680d046afc20ee2581f2076edfed1feb0
jerrykim91 <sun4131@gmail.com>
2020-03-30 14:59:22 +0900

    Simple_Game_2020.03.30

--- Reinforcement_Learning/Simple_Game.py
+++ Reinforcement_Learning/Simple_Game.py
@@ -1,60 +1,41 @@
-"""
+# 구조 
+'''
+- SlotMachineGame
+  L SlotArm Class
+  L GameEngine Class
+    L EpsilonGreedyEngine Class
+    L UCB1Engine Class
+  L GameSimulator Class or function
+'''
 
-# 전개 방향 
+# 필요 패키지 
+import numpy as np
+import pandas as pd
+import random
+import math
 
-- 개별 알고리즘 -> 탐색 처리 -> 그래픽스 -> 게임AI구현 -> 게임 구현 완료
+import matplotlib.pyplot as plt
+%matplotlib inline
 
-# AI
-- 약 AI (현재~)
-    - 하나의 업무만 잘한다. 
-    - 범용적
-- 강 AI (2040 도래) -
-- 초 AI (2060 도래)
+# Slot Machine Arm class
+class SlotArm():
+    # 초기화 -> 확률값 초기화(외부에서 주입)
+    # 생성자 함수 생성 ㄴ
+    def __init__(self,p):
+        # 맴버 변수 self.p가 만들어지고, 외부에서 입력된 p로 인해 값이 초기화 
+        self.p = p 
+        
+    # 팔 선택시 보상 지급 처리 -> 세팅된 확률보다 랜덤값이 적으면 -> 1.0
+    # 아니면 0.0  
+    def draw(self):
+        if 조건식 : 
+            return 1.0
+        else
+            return 0.0
+    pass
 
-# 다중 슬롯 머신 
+# 랜덤 값의 범위 확인?
+print([random.ran() for n in range(10)])
 
-- 알고리즘 
-    - UCB1
-    - ε-greedy (엡실론-greedy)
-
-
-# 미로 게임   
-
-- 알고리즘 
-    - 정책 경사법 
-    - Sarsa
-    - Q Learning
-
-
-# 카드 - 풀 게임 
-
-- DQN
-
-# 다중 슬롯 머신(Slot Machine) 구현
-
-- 스타일을 익힌다. 
-- 강화학습의 많은 요소들은 생략되어 있는 간단한 구조 
-- 전체적인 절차(플로우, 흐름)을 이해할 수 있다. 
-
-# 게임 환경 
-
-- 슬롯머신의 팔(Arm)은 여러개 존재할 수 있다. 
-- 각 Arm을 선택할때 보상 Reward가 나올 확율은 정해졌다.
-
-    - 1번 arm은 10%
-    - 2번 arm은 50%
-    - 3번 arm은 90% ...
-
-- 게임을 시작할 때 각 팔에 대한 확률을 모른다. 
-
-# 게임의 목적
-- 제한된 횟수에서 최대의 보상을 받는다.
-- 어떤 순서로 arm을 선택해야 하는가?
-- 어떤 arm을 당겨야 가장 많은 돈을 벌것인가?
-    - 단, 보상값들이 달라졌을 때 해당  
-
-# 에이전트의 행동 
-- 여러 슬롯중 한개를 선택
-- 1개를 당기면 -> 행동 -> 한 턴이 종료 
-
-"""
+# 게임 시뮬레이터 생성 
+# 1단계 
